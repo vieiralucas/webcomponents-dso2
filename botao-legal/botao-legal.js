8234a40c24a549da6d7760fbb9db6e3b19b02c7f
Lucas Vieira <vieiralucas4@gmail.com>
2015-06-29 02:24:31 -0300

    Finish botao-legal

--- botao-legal/botao-legal.js
+++ botao-legal/botao-legal.js
@@ -1,9 +1,33 @@
-var botaoLegal = Object.create(HTMLElement.prototype)
+var BotaoLegal = Object.create(HTMLElement.prototype)
 
-botaoLegal.createdCallback = function() {
-  this.textContent = "Meu Botão Legal"
+BotaoLegal.createdCallback = function() {
+  this.message = this.getAttribute('message') || "Meu Botao Legal"
+
+  if (this.attributes.background) {
+    this.style.background = this.getAttribute('background')
+  }
+
+  if (this.attributes.color) {
+    this.style.color = this.getAttribute('color')
+  }
+
+  if (this.attributes['border-color']) {
+    this.style.borderColor = this.getAttribute('border-color')
+  }
+
+  if (this.attributes.size) {
+    var size = this.getAttribute('size')
+    if (size === 'big') {
+      this.style.fontSize = '100px'
+    }
+  }
+
+  this.addEventListener('click', function() {
+    alert(this.message)
+  })
 }
 
-document.register('botao-legal', {
-  prototype: botaoLegal
+
+document.registerElement('botao-legal', {
+  prototype: BotaoLegal
 })
